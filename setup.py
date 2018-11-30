a9e4c66975a0b7483428fe436a19abd0b4f854bb
Souheil CHELFOUH <trollfot@gmail.com>
2018-12-01 00:41:41 +0100

    Fixing setup

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
         ],
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    namespace_packages=['dolmen'],
+    namespace_packages=['horseman'],
     include_package_data=True,
     zip_safe=False,
     install_requires=install_requires,
