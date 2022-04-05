306af52780ca552ef84e545633236550614fbc04
Souheil Chelfouh <trollfot@gmail.com>
2022-04-05 12:48:24 +0200

    Updated packaging

--- setup.py
+++ setup.py
@@ -1,51 +1,20 @@
-import os
-from setuptools import setup, find_packages
+from setuptools import setup
 
-version = "0.2"
-
-install_requires = [
-    'biscuits >= 0.3.0',
-    'multidict >= 5.1',
-    'multifruits >= 0.1.5',
-    'orjson >= 3.5',
-    'frozendict >= 2.0',
-]
-
-test_requires = [
-    'WebTest',
-    'pytest',
-    'pyhamcrest',
-]
 
 setup(
     name='horseman',
-    version=version,
-    author='Souheil CHELFOUH',
-    author_email='trollfot@gmail.com',
-    url='https://github.com/HorsemanWSGI/horseman',
-    download_url='http://pypi.python.org/pypi/horseman',
-    description='Headless WSGI API',
-    long_description=(
-        open("README.rst").read() + "\n" +
-        open(os.path.join("docs", "HISTORY.rst")).read()
-    ),
-    license='ZPL',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Zope Public License',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Topic :: Internet :: WWW/HTTP :: WSGI',
+    install_requires=[
+        'biscuits >= 0.3.0',
+        'multidict >= 5.1',
+        'multifruits >= 0.1.5',
+        'orjson >= 3.5',
+        'frozendict >= 2.0',
     ],
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=install_requires,
     extras_require={
-        'test': test_requires,
-        },
-    )
+        'test': [
+            'WebTest',
+            'pytest',
+            'pyhamcrest'
+        ]
+    }
+)
