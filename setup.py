78a34f1a321c8174c8e3547a95d4a868bdcc552d
Souheil Chelfouh <trollfot@gmail.com>
2026-03-24 11:18:10 +0100

    New version pin and package declaration

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup
 setup(
     name='horseman',
     install_requires=[
-        'biscuits >= 0.3.0',
-        'multidict >= 5.1',
-        'multifruits >= 0.1.5',
-        'orjson >= 3.5',
-        'frozendict >= 2.3',
+        'biscuits >= 0.3.2',
+        'multidict >= 6.7',
+        'multifruits >= 0.1.7',
+        'orjson >= 3.11',
+        'frozendict >= 2.4',
     ],
     extras_require={
         'test': [
